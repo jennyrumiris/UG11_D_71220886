08d2e9273f5106525219ed6467f368e0bb7bff9c
jennyrumiris <117902222+jennyrumiris@users.noreply.github.com>
2022-12-01 16:18:31 +0700

    Add files via upload

---
+++ 1_D_71220886.py
@@ -0,0 +1,34 @@
+
+
+print("Halo selamat datang di bioskop!")
+print("Dimanakah kamu ingin menonton?")
+print("1) XXI Empire")
+print("2) XXI Amplaz")
+print("3) XXI JCM")
+def pilihan1():
+    pilihan1 = int(input("masukkan pilihanmu:"))
+    nontonfilm = ("Mau nonton film apa nih? Ada film:")
+    film = ("1.Frozen 2.Keramat 3.KKN di Desa Penari")
+    print(nontonfilm)
+    print(film)
+pilihan1()
+def pilihan2():
+    pilihan3 = int(input("Pilih nomor film:"))
+    nontonlayar = ("Mau nonton layar bioskop apa:")
+    layar = ("1.Reguler 2.Dolby 3.Premiere")
+    print(nontonlayar)
+    print(layar)
+pilihan2()
+def pilihan3():
+    pilihan3 = int(input("Pilih nomor tipe layar:"))
+    tiket = int(input("berapa banyak tiket?"))
+    maujam = ("mau nonton jam berapa:")
+    jam = ("1.12.35 2.14.45 3.16.55 4.19.05")
+    print(maujam)
+    print(jam)
+pilihan3()
+pilihananda = int(input("masukkan angka pilihan Anda:"))
+berhasil = ("ok berhasil!, silahkan di nikmati di jam 12.35")
+print(berhasil)
+
+
